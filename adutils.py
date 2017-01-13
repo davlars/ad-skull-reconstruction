0cde28e43acb0e023a6b94557ebc29afa0b9772f
davlars <davlars@kth.se>
2017-01-13 12:38:28 +0100

    Added rebinning of data

--- adutils.py
+++ adutils.py
@@ -56,7 +56,7 @@ def get_discretization():
     return reco_space
 
 
-def get_ray_trafo(reco_space, use_subset=False):
+def get_ray_trafo(reco_space, use_subset=False, use_rebin=False, rebin_factor=10):
     # Geometry and forward projector
     if use_subset:
         turns = range(13, 16)
@@ -71,7 +71,10 @@ def get_ray_trafo(reco_space, use_subset=False):
 
     for turn in turns:
         print("Loading geometry for turn number {} out of {}".format(turn + 1, nTurns))
-        geomFile = os.path.join(data_path, (fileStart + 'Turn_' + str(turn) + '.geometry.p'))
+        if use_rebin:
+            geomFile = os.path.join(data_path, (fileStart + 'Turn_' + str(turn) + '_rebinFactor_' + str(rebin_factor) + '.geometry.p'))
+        else:
+            geomFile = os.path.join(data_path, (fileStart + 'Turn_' + str(turn) + '.geometry.p'))
 
         # Load pickled geometry (small workaround of incompatibility between Python2/Python3 in pickle)
         with open(geomFile, 'rb') as f:
@@ -98,7 +101,7 @@ def get_fbp(A):
     return fbp
 
 
-def get_data(A, use_subset=False):
+def get_data(A, use_subset=False, use_rebin=False, rebin_factor=10):
     # Data
     if use_subset:
         turns = range(13, 16)
@@ -112,7 +115,10 @@ def get_data(A, use_subset=False):
     imagesTurn = []
     for turn in turns:
         print("Loading data for turn number {} out of {}".format(turn + 1, nTurns))
-        dataFile = os.path.join(data_path, (fileStart + 'Dose150mGy_Turn_' + str(turn) + '.data.npy'))
+        if use_rebin:
+            dataFile = os.path.join(data_path, (fileStart + 'Dose150mGy_Turn_' + str(turn) + '_rebinFactor_' + str(rebin_factor) + '.data.npy'))
+        else:
+            dataFile = os.path.join(data_path, (fileStart + 'Dose150mGy_Turn_' + str(turn) + '.data.npy'))
         projections = np.load(dataFile).astype('float32')
         imagesTurn.append(-np.log(projections / 7910))
 
@@ -263,3 +269,57 @@ def plot_data(x, phantomName='70100644Phantom_xled_no_bed.nii', plot_separately=
             """
     else:
         print('Phantom not recognized. Has it really been simulated?')
+
+def rebin_data(rebin_factor=10, plot_rebin=False):   
+    
+    if 4000 % rebin_factor:
+        raise IOError('Cannot rebin data %i projections with rebin factor %i' %(4000,rebin_factor))
+    
+    if not os.path.exists(data_path):
+        raise IOError('Could not find files at {}, have you run '
+                      'adutils.load_data_from_nas()'.format(data_path))
+    for turn in range(23):
+        myFile = os.path.join(data_path,(fileStart + 'Dose150mGy_Turn_' + str(turn) + '.data.npy'))
+        print("Rebinning data for turn number {} out of {}".format(turn + 1, nTurns))
+        projections = np.load(myFile)
+        size = np.array(np.shape(projections))
+        sizeNew = size
+        sizeNew[0] /= rebin_factor
+        projection_rebin = np.empty(sizeNew)
+        for i in range(int(size[0])):
+            singleBin = np.mean(projections[i*10:(i+1)*10,:,:], axis = 0)
+            projection_rebin[i,...] = singleBin
+        saveName = os.path.join(data_path,(fileStart + 'Dose150mGy_Turn_' + str(turn) + '_rebinFactor_'+str(rebin_factor) + '.data.npy'))
+        np.save(saveName,projection_rebin)
+        
+        print("Rebinning geometry for turn number {} out of {}".format(turn + 1, nTurns))
+        geomFile = os.path.join(data_path,(fileStart + 'Turn_' + str(turn) + '.geometry.p'))        
+        with open(geomFile, 'rb') as f:
+            if PY3:
+                geom = pickle.load(f, encoding='latin1')
+            else:
+                geom = pickle.load(f)
+        
+        nProjection = sizeNew[0]
+        angle_partition = odl.uniform_partition(2 * np.pi * turn, 
+                                                2 * np.pi * (turn + 1), 
+                                                nProjection)
+        geom_rebin = odl.tomo.HelicalConeFlatGeometry(angle_partition, 
+                                                    geom.det_partition, 
+                                                    src_radius=geom.src_radius, 
+                                                    det_radius=geom.det_radius,
+                                                    pitch=geom.pitch,
+                                                    pitch_offset=-geom.pitch_offset)    
+        
+        pickle.dump(geom_rebin, open(os.path.join(data_path,(fileStart + 'Turn_' + str(turn) + '_rebinFactor_' + str(rebin_factor) + '.geometry.p')), 'wb+'))        
+       
+        if turn == 0:
+            projectionsTot = projection_rebin
+        else:
+            projectionsTot = np.append(projectionsTot,projection_rebin,axis = 0)
+    
+    if plot_rebin == True:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(projectionsTot[:,:,10])
+        ax.set_aspect('auto')
