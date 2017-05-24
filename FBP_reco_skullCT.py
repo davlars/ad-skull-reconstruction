fc4d863323c09ec257e4c0e4f0e93b05a7a3b5ac
davlars <davlars@kth.se>
2017-05-24 23:26:33 +0200

    Update FBP_reco_skullCT.py

--- FBP_reco_skullCT.py
+++ FBP_reco_skullCT.py
@@ -26,7 +26,7 @@ x.show(coords=[0, None, None])
 x.show(coords=[None, None, 90])
 
 # Save
-saveReco = False:
+saveReco = False
 if saveReco:
     saveName = '/home/user/Simulated/120kV/reco/Reco_HelicalSkullCT_70100644Phantom_no_bed_Dose150mGy_FBP.npy'
     np.save(saveName, np.asarray(x))
